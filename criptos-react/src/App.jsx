36e36971ecbb187042b6503f65f0e23c0ced30b7
manuelduarte077 <carlosd99m@gmail.com>
2022-08-15 01:31:49 -0600

    feat: add styled components

--- criptos-react/src/App.jsx
+++ criptos-react/src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import React from 'react'
+import styled from '@emotion/styled'
 
+// Definir styled components
+const Heading = styled.h1`
+    font-family: 'Lato', sans-serif;
+    color: #fff;
+`
 
 function App() {
-
-  return (
-    <h1>From App</h1>
-  )
+    return <Heading>From App</Heading>
 }
 
 export default App
