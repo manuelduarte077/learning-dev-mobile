cf9b95f6f3e3167f3eea1550ff49a38bf6ff31de
manuelduarte077 <carlosd99m@gmail.com>
2022-08-20 14:43:52 -0600

    querying the weather in the API

--- weatherapp/src/context/ClimaProvider.jsx
+++ weatherapp/src/context/ClimaProvider.jsx
@@ -1,10 +1,12 @@
 import {useState} from 'react'
 import {createContext} from 'react'
 
+import ky from 'ky'
+
 const ClimaContext = createContext()
 
 const ClimaProvider = ({children}) => {
-  const [busqueda] = useState({
+  const [busqueda, setBusqueda] = useState({
     ciudad: '',
     pais: '',
   })
@@ -16,8 +18,25 @@ const ClimaProvider = ({children}) => {
     })
   }
 
-  const consultarClima = (datos) => {
-    console.log(datos)
+  const consultarClima = async (datos) => {
+    try {
+      const {ciudad, pais} = datos
+
+      const appId = import.meta.env.VITE_API_KEY
+
+      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${appId}`
+
+      const data = await ky.get(url).json()
+
+      const {lat, lon} = data[0]
+
+      const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
+
+      const clima = await ky.get(urlClima).json()
+      console.log(clima)
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
