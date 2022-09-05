77b195d9c71b0c2504c2c5947bfb50b9001ac8a4
manuelduarte077 <carlosd99m@gmail.com>
2022-09-04 22:56:31 -0600

    fix: navigation

--- RNGoku/App.js
+++ RNGoku/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-import HomeScreen from './src/screens/HomeScreen'
-
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import HomeScreen from './src/screens/HomeScreen'
+import DetailsScreen from './src/screens/DetailsScreen'
 
 const Stack = createNativeStackNavigator()
 
@@ -12,7 +13,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
--- RNGoku/src/screens/DetailsScreen.js
+++ RNGoku/src/screens/DetailsScreen.js
@@ -1,4 +1,5 @@
-import { View } from 'react-native'
+import React from 'react'
+import { View, Text } from 'react-native'
 
 const DetailsScreen = () => {
   return (
