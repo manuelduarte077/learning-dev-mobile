ac90a672c5ce802cbd0918206f944c0733fa3806
Manuel Duarte  <carlosd99m@gmail.com>
2023-01-22 11:38:21 -0600

    react rick and morty

--- react-rick-and-morty/src/App.jsx
+++ react-rick-and-morty/src/App.jsx
@@ -1,34 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import CharacterList from "./components/CharacterList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="bg-dark text-white">
+      <h1 className="text-center display-1 py-4">Rick And Morty</h1>
+      <CharacterList />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
