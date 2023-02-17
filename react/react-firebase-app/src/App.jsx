16e4aec528c9fa7cf5ba78556351d2d588bcb42e
Manuel Duarte  <carlosd99m@gmail.com>
2023-02-16 22:31:58 -0600

    add react-router-dom

--- react/react-firebase-app/src/App.jsx
+++ react/react-firebase-app/src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import { Routes, Route } from "react-router-dom";
 
-export default App;
+import { Home } from "./components/Home";
+import { Register } from "./components/Register";
+import { Login } from "./components/Login";
+
+export default function App() {
+  return (
+    <div className="bg-slate-300 h-screen text-white flex">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
---
+++ react/react-firebase-app/src/components/ProtectedRoutes.jsx
@@ -0,0 +1,3 @@
+export function ProtectedRoutes() {
+  return <div>ProtectedRoutes</div>;
+}
