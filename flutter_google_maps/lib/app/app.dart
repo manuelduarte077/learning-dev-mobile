0b7836a72228e2abb20b9b0e4a28fcc45f5dd737
Manuel Duarte  <carlosd99m@gmail.com>
2023-03-27 00:20:15 -0600

    refactor code

--- flutter_google_maps/lib/app/app.dart
+++ flutter_google_maps/lib/app/app.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_google_maps/screens/01_standard_markers_screen.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: StandardMarkersScreen(),
       ),
     );
   }
