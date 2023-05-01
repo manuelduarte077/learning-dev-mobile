406fa13084dbcc2a23b708033556f54fb9d5dc8b
Manuel Duarte  <carlosd99m@gmail.com>
2023-05-01 15:57:34 -0600

    feat: example of how to use lottie in android

--- LottieDemo/app/src/main/java/io/manuelduarte077/lottiedemo/MainScreen.kt
+++ LottieDemo/app/src/main/java/io/manuelduarte077/lottiedemo/MainScreen.kt
@@ -3,21 +3,65 @@ package io.manuelduarte077.lottiedemo
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 
 @Composable
 fun MainScreen() {
+
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.lottie_demo)
+    )
+    var isPlaying by remember { mutableStateOf(true) }
+
+    val progress by animateLottieCompositionAsState(
+        composition,
+        isPlaying = isPlaying,
+    )
+
+    LaunchedEffect(key1 = progress) {
+        if (progress == 0f) {
+            isPlaying = true
+        }
+
+        if (progress == 1f) {
+            isPlaying = false
+        }
+    }
+
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Button(onClick = { /*TODO*/ }) {
+        LottieAnimation(
+            composition,
+            modifier = Modifier.size(300.dp),
+            progress = { progress },
+        )
+        Button(onClick = { isPlaying = true }) {
             Text(text = "Play Again")
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    MainScreen()
 }
