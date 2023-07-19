c1b3949e1c4376a7d9f061fa73558eb659cd8637
Manuel Duarte  <carlosd99m@gmail.com>
2023-07-19 12:49:05 -0600

    chore(packages): install packages

--- flutter/nba_app/lib/main.dart
+++ flutter/nba_app/lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nba_app/app/main.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const ProviderScope(child: MyApp()));
+}
