d0b2d7941b050d3187e0e3c2851200ffe3127664
Manuel Duarte  <carlosd99m@gmail.com>
2023-07-20 20:27:02 -0600

    remove platform desktop

--- flutter/architecture_challenge/android/app/src/main/kotlin/com/example/architecture_challenge/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.architecture_challenge
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- flutter/architecture_challenge/generate_code.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-flutter clean
-flutter pub upgrade
-# https://flutter.dev/docs/development/data-and-backend/json#generating-code-continuously
-dart run build_runner watch --delete-conflicting-outputs
