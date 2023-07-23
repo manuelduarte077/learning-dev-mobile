0e625933245e5ed1325f7695a0bdfdade1691934
Manuel Duarte  <carlosd99m@gmail.com>
2023-07-22 18:01:11 -0600

    movie info create ui

---
+++ kotlin/MovieInformation/app/src/main/java/io/manuelduarte077/movieinformation/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.manuelduarte077.movieinformation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
