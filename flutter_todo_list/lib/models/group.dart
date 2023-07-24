784e170ee3d47a0c32f1838ab99b8b8d374b9c08
Manuel Duarte  <carlosd99m@gmail.com>
2023-07-24 12:15:16 -0600

    initial setup

---
+++ flutter_todo_list/lib/models/group.dart
@@ -0,0 +1,10 @@
+class Group {
+  int id = 0;
+  String name;
+  int color;
+
+  Group({
+    required this.name,
+    required this.color,
+  });
+}
---
+++ flutter_todo_list/lib/models/task.dart
@@ -0,0 +1,9 @@
+class Task {
+  int id = 0;
+  String description;
+  bool completed = false;
+
+  Task({
+    required this.description,
+  });
+}
