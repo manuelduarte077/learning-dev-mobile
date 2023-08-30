77297d883caafd6f4a865bb8ee37be1bf82d9df9
Manuel Duarte  <carlosd99m@gmail.com>
2023-08-30 01:36:33 -0600

    add resources

---
+++ kotlin/Superheroes/app/src/main/java/com/manuelduarte077/superheroes/model/Hero.kt
@@ -0,0 +1,10 @@
+package com.manuelduarte077.superheroes.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Hero(
+    @StringRes val nameRes: Int,
+    @StringRes val descriptionRes: Int,
+    @DrawableRes val imageRes: Int
+)
