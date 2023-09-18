8418e2dd3a63a039a259154fa630df1a43d56c22
Manuel Duarte  <carlosd99m@gmail.com>
2023-09-17 22:29:16 -0600

    initial code

---
+++ flutter-bloc/lib/presentation/screens/01_simple_cubit/cubit_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CubitScreen extends StatelessWidget {
+  const CubitScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Cubit'),
+      ),
+      body: const Center(
+        child: Text('Fernando Herrera'),
+      ),
+    );
+  }
+}
